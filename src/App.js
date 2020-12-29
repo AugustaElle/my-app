9cc6fd981dd344ccaf55a4c6142e3c9839acd1b7
AugustaElle <68307752+AugustaElle@users.noreply.github.com>
2020-12-29 07:44:22 -0500

    Added code and axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  let [city, setCity] = useState(null);
+  let [temperature, setTemperature] = useState(null);
+
+
+  function showTemp(response) {
+    setTemperature(
+      <ul>
+        <li>Temp: {Math.round(response.data.main.temp)}°C</li>
+        <li>Humidity: {Math.round(response.data.main.humidity)}%</li>
+        <li>Description: {response.data.weather[0].description}</li>
+        <li>Wind: {response.data.wind.speed}km/h</li>
+        <li>
+          <img
+            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+          />
+        </li>
+      </ul>
+    );
+  }
+
+  function displayCity(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9bcab27e2586a82d6ff0a45c0b3f6c89&units=metric`;
+    axios.get(url).then(showTemp);
+  }
+
+  function inputCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather App </h1>
+      <form onSubmit={displayCity}>
+        <input type="text" placeholder="Enter a city" onChange={inputCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h1>{temperature}</h1>
     </div>
   );
 }
 
-export default App;
